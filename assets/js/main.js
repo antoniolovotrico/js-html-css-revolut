bcafca797c34eeee8778effb476813b5a0841711
Antonio <antonio.lovotrico@icloud.com>
2020-11-30 22:30:17 +0100

    refactoring JS code

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,14 +8,7 @@ $(function(){
         
         $(this).addClass("hover");
         $('ul:first',this).css('visibility', 'visible');
-
-    }, function(){
-
-        $(this).removeClass("hover");
-        $('ul:first',this).css('visibility', 'hidden');
         
     });
-    
-    $("ul.drop_down li ul li:has(ul)").find("a:first");
 
 });
