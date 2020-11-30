bf1d820bff0bbdfb0c7b3aa36d310f299003cb77
Antonio <antonio.lovotrico@icloud.com>
2020-11-30 22:32:55 +0100

    unset some previous change

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,14 @@ $(function(){
         
         $(this).addClass("hover");
         $('ul:first',this).css('visibility', 'visible');
+
+    }, function(){
+
+        $(this).removeClass("hover");
+        $('ul:first',this).css('visibility', 'hidden');
         
     });
+    
+    $("ul.drop_down li ul li:has(ul)").find("a:first");
 
 });
