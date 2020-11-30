7c94a72f9e3971ba170bce50afdde4bde499a8cb
Antonio <antonio.lovotrico@icloud.com>
2020-11-30 19:56:22 +0100

    set jquery property for dropdown

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+//Handler Metod
+$(function(){
+    //definire variabile pulsante menu
+    
+    
+    //funzione dropdown menu
+    $("ul.drop_down li").hover(function(){
+        
+        $(this).addClass("hover");
+        $('ul:first',this).css('visibility', 'visible');
+
+    }, function(){
+
+        $(this).removeClass("hover");
+        $('ul:first',this).css('visibility', 'hidden');
+        
+    });
+    
+    $("ul.drop_down li ul li:has(ul)").find("a:first").append(" &raquo; ");
+
+});
